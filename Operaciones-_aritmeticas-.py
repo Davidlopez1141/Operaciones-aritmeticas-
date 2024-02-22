76e92bd23c96bd7c8a0987954b20ecc1eb6ffe0e
davidlopez1141 <davidlopez@colegioguanenta.edu.co>
2024-02-22 17:35:54 -0500

    se realizo analisis y diseño

---
+++ Operaciones-_aritmeticas-.py
@@ -0,0 +1,26 @@
+# programma para calcular la suma,resta,multiplicacion,division,division entera
+
+# inpunt 
+x= int (input("digita el valor de x."))
+y= int (input("digita el valor de y."))
+
+# procesing 
+
+s= (x+y)
+r= (x-y)
+m= (x*y)
+d= (x/y)
+de= (x//y) 
+mod= (x%y)
+p= (x**y)
+
+# ouput 
+
+print ("este es el resultado de los numeros dados para las operaciones:")
+print ("Suma:",s)
+print ("Resta:",r)
+print ("Multiplicacion:",m)
+print ("Division",d)
+print ("Division entera:",de)
+print ("Modulo".mod)
+print ("Potencia",p)
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Operaciones-aritmeticas-
 programa para calcular las operaciones artmeticas 
+
+# calcular mediante 2 numeros enteros distintas operaciones.
+
+
+# Analisis 
+
+-Variable de entrada 
+X y Y:Es igual a dos numeros naturales,para desarrollar las operaciones aritmeticas correspondientes.
+
+-Variable de salida 
+
+s: x+y
+r: x-y 
+m: x*y
+d: x/y
+de: x//y
+mod: x%y
+p: x**y
+
+
+# Diseño 
+¡[Diagrama de flujo](diagrama.png"diagrama de flujo")
+
+# Construccion 
+
+
